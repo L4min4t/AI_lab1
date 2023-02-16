aa93d79f585366777628bf0084ff1fd8974e6bc5
Ivan Vlasiuk <vlasiukk.i.v@gmail.com>
2023-02-16 17:23:38 +0200

    Add project files.

---
+++ AI_lab1/LogicFunctions.cs
@@ -0,0 +1,35 @@
+﻿namespace AI_lab1
+{
+    public class LogicFunctions
+    {
+        public static int AND(int a, int b)
+        {
+            return _weightedAmount(a, 1, b, 1) >= 1.5 ? 1 : 0;
+        }
+
+        public static int OR(int a, int b)
+        {
+            return _weightedAmount(a, 1, b, 1) >= 0.5 ? 1 : 0;
+        }
+
+        public static int NOT(int a)
+        {
+            return _weightedAmount(a, -1.5) >= -1 ? 1 : 0;
+        }
+
+        public static int XOR(int a, int b)
+        {
+            int y1 = _weightedAmount(a, 1, b, -1) >= 0.5 ? 1 : 0;
+            int y2 = _weightedAmount(a, -1, b, 1) >= 0.5 ? 1 : 0;
+            return _weightedAmount(y1, 1, y2, 1) >= 0.5 ? 1 : 0;
+        }
+
+        private static double _weightedAmount(int a, double weightA, int b = 0, double weightB=0)
+        {
+            return a * weightA + b * weightB;
+        }
+
+
+
+    }
+}
---
+++ AI_lab1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AI_lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            while(true)
+            {
+                Console.WriteLine("Choose 1-and, 2-or, 3-xor, 4-not, and enter the two Boolean values as integers.");
+                int choose, a, b;
+                var numbers = Console.ReadLine().Split(' ');
+                choose = int.Parse(numbers[0]);
+                a = int.Parse(numbers[1]);
+                b = int.Parse(numbers[2]);
+                switch (choose)
+                {
+                    case 1:
+                        Console.WriteLine($"result: {LogicFunctions.AND(a, b)}");
+                        break;
+
+                    case 2:
+                        Console.WriteLine($"result: {LogicFunctions.OR(a, b)}\n");
+                        break;
+
+                    case 3:
+                        Console.WriteLine($"result: {LogicFunctions.XOR(a, b)}\n");
+                        break;
+
+                    case 4:
+                        Console.WriteLine($"result: {LogicFunctions.NOT(a)}\n");
+                        break;
+                }
+            }
+            
+        }
+    }
+}
